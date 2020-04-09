667f5f1eec6f9430f5ac3eb0c5727d9765bee17f
xenahort <juan.carlos.wow.95@gmail.com>
2020-04-09 18:52:20 +0200

    Better error handling

--- src/main/ErrorHandler.java
+++ src/main/ErrorHandler.java
@@ -1,31 +1,16 @@
 package main;
 
+import java.util.ArrayList;
+
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class ErrorHandler extends DefaultHandler {
 	
-	public void warning(SAXParseException e) throws SAXException {
-		System.out.println("Warning: ");
-		printInfo(e);
-	}
-
-	public void error(SAXParseException e) throws SAXException {
-		System.out.println("Error: ");
-		printInfo(e);
-	}
-
-	public void fatalError(SAXParseException e) throws SAXException {
-		System.out.println("Fattal error: ");
-		printInfo(e);
-	}
-
-	private void printInfo(SAXParseException e) {
-		System.out.println("   Public ID: " + e.getPublicId());
-		System.out.println("   System ID: " + e.getSystemId());
-		System.out.println("   Line number: " + e.getLineNumber());
-		System.out.println("   Column number: " + e.getColumnNumber());
-		System.out.println("   Message: " + e.getMessage());
+	public ArrayList<String> errors;
+	
+	public ErrorHandler(){
+		this.errors = new ArrayList<String>();
 	}
 }
--- src/main/Validate.java
+++ src/main/Validate.java
@@ -32,42 +32,60 @@ public class Validate {
 
 	private final static Logger LOGGER = Logger.getLogger("bitacora.subnivel.Control");
 
+	// Imput files
+	private final static String XSDpath = "C:/Users/jserrape/Documents/GitHub/XSD-Validation/src/files/students.xsd";
+	private final static String XMLpath = "C:/Users/jserrape/Documents/GitHub/XSD-Validation/src/files/students.xml";
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		LOGGER.log(Level.INFO, "Start of execution");
 
-		String XSDpath = "C:/Users/jserrape/Documents/GitHub/XSD-Validation/src/files/students.xsd";
-		String XMLpath = "C:/Users/jserrape/Documents/GitHub/XSD-Validation/src/files/students.xml";
-
-
 		System.out.println("XML path:".concat(XMLpath));
 		System.out.println("XSD path:".concat(XSDpath));
 		System.out.println("");
-		
+
 		try {
 			SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 			Schema schema = factory.newSchema(new File(XSDpath));
 			Validator validator = schema.newValidator();
+			ErrorHandler handler = new ErrorHandler() {
+				@Override
+				public void warning(SAXParseException exception) throws SAXException {
+					System.out.println(exception);
+				}
+
+				@Override
+				public void fatalError(SAXParseException exception) throws SAXException {
+					System.out.println(exception);
+				}
+
+				@Override
+				public void error(SAXParseException exception) throws SAXException {
+					if (!exception.getMessage().contains("cvc-type")) {
+						String reasonAux = "Error in the line " + exception.getLineNumber() + " and the column "
+								+ exception.getColumnNumber() + "Reason: " + exception.getMessage();
+						// System.out.println(reasonAux);
+						this.errors.add(reasonAux);
+					}
+				}
+			};
+			validator.setErrorHandler(handler);
 			validator.validate(new StreamSource(new File(XMLpath)));
+
+			if (!handler.errors.isEmpty()) {
+				LOGGER.log(Level.SEVERE, "There are " + handler.errors.size() + " errors:");
+				for (String err : handler.errors) {
+					LOGGER.log(Level.SEVERE, err);
+				}
+			} else {
+				LOGGER.log(Level.SEVERE, "The execution finished without errors");
+			}
 		} catch (IOException e) {
 			LOGGER.log(Level.SEVERE, "Exception: " + e.getMessage());
-		} catch (SAXParseException e) {
-            int line = e.getLineNumber();
-            int col = e.getColumnNumber();
-            String message = e.getMessage();
-            System.out.println("Error when validate XML against XSD Schema\n" +
-                    "line: " + line + "\n" +
-                    "column: " + col + "\n" +
-                    "message: " + message.substring(message.indexOf(":") + 2));
-        } catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (SAXException e) {
+			LOGGER.log(Level.SEVERE, "SAX Exception: " + e.getMessage());
 		}
-		
-		System.out.println("");
-		LOGGER.log(Level.INFO, "Finish ok");
 	}
-
 }
